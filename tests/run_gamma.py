94c2a966c4eac755e7a9b7fa1f94bc998f2c5883
Hung-Jin <arcspatio@gmail.com>
2021-04-11 15:05:05 -0700

    minor

---
+++ tests/run_gamma.py
@@ -0,0 +1,33 @@
+from get_pars import get_pars0
+from tfCube2 import Parameters
+import sys
+import pathlib
+import numpy as np
+import time
+import emcee
+from multiprocessing import Pool
+
+dir_repo = str(pathlib.Path(__file__).parent.absolute())+'/../..'
+dir_KLens = dir_repo + '/KLens'
+sys.path.append(dir_KLens)
+dir_binnedFit = dir_repo + '/BinnedFit'
+sys.path.append(dir_binnedFit)
+sys.path.append(dir_binnedFit+'/tests')
+from get_pars import get_pars0
+from gen_mocks import gen_mock_tfCube
+from gamma import GammaInference
+from binnedFit_utilities import save_pickle
+
+pars, _ = get_pars0()
+dataInfo = gen_mock_tfCube(pars, 'Halpha', slits='both', noise_mode=0)
+
+# ----------------------
+GI = GammaInference(dataInfo, active_par_key=[
+    'vcirc', 'sini', 'vscale', 'r_0', 'v_0', 'g1', 'g2',  'r_hl_image', 'theta_int', 'flux'], par_fix=None, vTFR_mean=200.)
+
+chainInfo = GI.run_MCMC(Nwalker=30, Nsteps=1)
+
+filename = "./chain_Ha_noise0.pkl"
+
+save_pickle(filename, chainInfo)
+
